fac6c6a7ef667b09f1901c21bcbde1c8c1eced5f
Edimega <86736545+Edimega@users.noreply.github.com>
2024-09-19 02:00:41 -0500

    Create proyect

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/* import { useState } from 'react' */
+import './assets/js/tailwind.js'
+
+function App() {
+
+	return (
+		<>
+			<div>
+				<h1>Tik Tok</h1>
+			</div>
+		</>
+	)
+}
+
+export default App
+
+/* https://youtu.be/cf4UnP7B-vo?si=WGtnGGs0UwAiHy2x&t=1052 */
